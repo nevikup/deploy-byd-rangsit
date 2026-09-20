b2d4543eba262157c4301dc3c1cd1d6d72544cda
DESKTOP-J77BNUC\Admin <nevikup@gmail.com>
2026-09-20 10:10:54 +0700

    deploy all  models

--- server/_ssr/index.mjs
+++ server/_ssr/index.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 let serverEntryPromise;
 async function getServerEntry() {
   if (!serverEntryPromise) {
-    serverEntryPromise = import("./server-DpvJsaob.mjs").then((n) => n.s).then(
+    serverEntryPromise = import("./server-Cd8gvDPe.mjs").then((n) => n.s).then(
       (m) => m.default ?? m
     );
   }
